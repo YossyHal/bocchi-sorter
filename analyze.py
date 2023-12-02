6a2da6f596884ab3371a6d2fac5dd49b2d9f014e
YossyHal <yossyhal.k@gmail.com>
2023-12-02 12:22:08 +0900

    ネガポジのスコア順にソート

--- analyze.py
+++ analyze.py
@@ -1,14 +1,31 @@
+import pprint
+from pathlib import Path
+
 import oseti
 
-with open('text/青春コンプレックス.txt', 'r') as f:
-    lyric = f.read()
-    print(lyric)
 
-analyzer = oseti.Analyzer()
-results  = analyzer.analyze(lyric)
-print(results)
+def main():
+    p = Path("text")
+    d = dict()
+    for path in p.glob("*.txt"):
+        score = get_lyric_score(path)
+        d[path.stem] = score
+    d = sorted(d.items(), key=lambda x: x[1])
+    pprint.pprint(d)
 
-total_score = 0
-for result in results:
-    total_score += result
-print(total_score)
+
+def get_lyric_score(path):
+    with open(path, "r") as f:
+        lyric = f.read()
+        analyzer = oseti.Analyzer()
+        results = analyzer.analyze(lyric)
+        # print(results)
+
+        total_score = 0
+        for result in results:
+            total_score += result
+        return total_score
+
+
+if __name__ == "__main__":
+    main()
